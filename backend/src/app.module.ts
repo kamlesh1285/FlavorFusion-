7f9d5e030650dfb98853f3ed7822d4451ae891cd
kamlesh1285 <kamlesh1285ky@gmail.com>
2026-07-11 06:39:58 +0530

    Add NestJS backend and PostgreSQL configuration

---
+++ backend/src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+
+        host: configService.get<string>('DATABASE_HOST'),
+
+        port: Number(configService.get<number>('DATABASE_PORT')),
+
+        username: configService.get<string>('DATABASE_USERNAME'),
+
+        password: configService.get<string>('DATABASE_PASSWORD'),
+
+        database: configService.get<string>('DATABASE_NAME'),
+
+        autoLoadEntities: true,
+
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class AppModule {}
